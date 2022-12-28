9d29d2c844324267a30cc40cb18e1daa2b4f179a
Gulhuseyn313 <a.gulhuseyn2005@gmail.com>
2022-12-28 07:17:48 +0400

    task for dom#1

--- 17.12.2022/app.js
+++ 17.12.2022/app.js
@@ -1,27 +1,27 @@
-//-----------------------------------------------------Task 1-----------------------------------------------------
-// const menfiededlerisech = (arr) => {
-//     const ededler = []
-//     for (let i = 0; i < arr.length; i++) {
-//         if (arr[i] < 0) {
-//             ededler.push(arr[i])
-//         }
-//     } return ededler
-// }
-// console.log(menfiededlerisech([1, 2, -3, -4, 5, 6, -7, -8, 9]))
+// -----------------------------------------------------Task 1-----------------------------------------------------
+const menfiededlerisech = (arr) => {
+    const ededler = []
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] < 0) {
+            ededler.push(arr[i])
+        }
+    } return ededler
+}
+console.log(menfiededlerisech([1, 2, -3, -4, 5, 6, -7, -8, 9]))
 
-//-----------------------------------------------------Task 3-----------------------------------------------------
+// -----------------------------------------------------Task 3-----------------------------------------------------
 
 
-//  const names = ["Michael", "Trevor", "Franklin", "Lamar", "Jimmy"]
-//     names.forEach((elem, index) => {
-//         console.log(index, elem)
-//     })
+ const names = ["Michael", "Trevor", "Franklin", "Lamar", "Jimmy"]
+    names.forEach((elem, index) => {
+        console.log(index, elem)
+    })
 
-//-----------------------------------------------------Task 4-----------------------------------------------------
-// const kebabToSnake = (str) => str.replaceAll('-', '_').toUpperCase()
-// console.log(kebabToSnake('ali-greenheart is pramoy-heart'))
+// -----------------------------------------------------Task 4-----------------------------------------------------
+const kebabToSnake = (str) => str.replaceAll('-', '_').toUpperCase()
+console.log(kebabToSnake('ali-greenheart is pramoy-heart'))
 // --------------------------------------------------------------------------------------
-// const snakeToKebab = (str) => str.replaceAll('_', '-').toLowerCase()
-// console.log(snakeToKebab('JAVASCRIPT_IS_HELL'))
+const snakeToKebab = (str) => str.replaceAll('_', '-').toLowerCase()
+console.log(snakeToKebab('JAVASCRIPT_IS_HELL'))
 
-//-----------------------------------------------------Task 5-----------------------------------------------------
+// -----------------------------------------------------Task 5-----------------------------------------------------
---
+++ 27.12.2022/app.js
@@ -0,0 +1,22 @@
+
+const btnelemone = document.querySelector( '#blue')
+btnelemone.onclick = () => {
+    document.body.style.backgroundColor = 'blue'
+    alert("Do you want blue?")
+}
+const btnelemtwo = document.querySelector('#gray')
+btnelemtwo.onclick = () => {
+    document.body.style.backgroundColor = 'gray'
+    alert("Do you want gray?")
+}
+const btnelemthree = document.querySelector('#red')
+btnelemthree.onclick = () => {
+    document.body.style.backgroundColor = 'red'
+    alert("Do you want red?")
+    
+}
+const btnelemfour = document.querySelector('#yellow')
+btnelemfour.onclick = () => {
+    document.body.style.backgroundColor = 'yellow'
+    alert("Do you want yellow?")
+}
